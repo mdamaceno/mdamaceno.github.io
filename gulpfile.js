86116e57b8e88fecfaab4147208952d4181c5ec5
Marco Damaceno <maadamaceno@gmail.com>
2017-04-26 00:31:33 -0300

    Webserver with hot reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,50 @@
 var gulp = require('gulp');
 var handlebars = require('gulp-compile-handlebars');
 var rename = require('gulp-rename');
+var concatCss = require('gulp-concat-css');
+var concatJs = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
+var util = require('gulp-util');
 var del = require('del');
+var webserver = require('gulp-webserver');
+
+var production = !!util.env.production;
+
+gulp.task('webserver', () => {
+    gulp.src('./dist')
+        .pipe(webserver({
+            livereload: true,
+            directoryListing: false,
+            open: false
+        }));
+})
+
+gulp.task('css', () => {
+    // build CSS
+    del(['./dist/assets/css/**/*.css'])
+    gulp
+        .src('./src/assets/css/**/*.css')
+        .pipe(concatCss('bundle.css'))
+        .pipe(production ? cleanCSS({ compatibility: 'ie8' }) : util.noop())
+        .pipe(gulp.dest('./dist/assets/css'))
+})
+
+gulp.task('js', () => {
+    // build JS
+    del(['./dist/assets/js/**/*.js'])
+    gulp
+        .src('./src/assets/js/**/*.js')
+        .pipe(concatJs('bundle.js'))
+        .pipe(production ? uglify() : util.noop())
+        .pipe(gulp.dest('./dist/assets/js'))
+})
 
 gulp.task('html', () => {
-    return gulp.src('./src/pages/*.hbs')
+    // build HTML
+    del(['./dist/**/*.html'])
+    gulp
+        .src('./src/*.hbs')
         .pipe(handlebars({}, {
             ignorePartials: true,
             batch: ['./src/partials']
@@ -14,6 +54,14 @@ gulp.task('html', () => {
         }))
         .pipe(gulp.dest('./dist'));
 });
+
+gulp.task('watch', function() {
+    gulp.watch('./src/assets/css/**/*.css', ['css']);
+    gulp.watch('./src/assets/js/**/*.js', ['js']);
+    gulp.watch('./src/**/*.hbs', ['html']);
+})
+
+gulp.task('default', ['css', 'js', 'html', 'webserver', 'watch']);
 
 gulp.task('clear', function() {
     del([
