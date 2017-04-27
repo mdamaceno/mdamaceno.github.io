84038bedfd9b5097b683968d021f3a7ea74bf9ca
Marco Damaceno <maadamaceno@gmail.com>
2017-04-27 01:45:47 -0300

    rebuild for inclusion og pug

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,22 @@
-var gulp = require('gulp');
-var handlebars = require('gulp-compile-handlebars');
-var rename = require('gulp-rename');
-var concatCss = require('gulp-concat-css');
-var concatJs = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var cleanCSS = require('gulp-clean-css');
-var util = require('gulp-util');
-var del = require('del');
-var webserver = require('gulp-webserver');
+const gulp = require('gulp')
+const pug = require('gulp-pug')
+const del = require('del')
+const concatCss = require('gulp-concat-css');
+const concatJs = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const cleanCSS = require('gulp-clean-css');
+const util = require('gulp-util');
 
 var production = !!util.env.production;
 
-gulp.task('webserver', () => {
-    gulp.src('./dist')
-        .pipe(webserver({
-            livereload: true,
-            directoryListing: false,
-            open: false
-        }));
+gulp.task('build', ['pug', 'css', 'js'])
+
+gulp.task('pug', () => {
+    gulp.src(['./src/**/*.pug'])
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./dist'))
 })
 
 gulp.task('css', () => {
@@ -39,32 +38,3 @@ gulp.task('js', () => {
         .pipe(production ? uglify() : util.noop())
         .pipe(gulp.dest('./dist/assets/js'))
 })
-
-gulp.task('html', () => {
-    // build HTML
-    del(['./dist/**/*.html'])
-    gulp
-        .src('./src/*.hbs')
-        .pipe(handlebars({}, {
-            ignorePartials: true,
-            batch: ['./src/partials']
-        }))
-        .pipe(rename({
-            extname: '.html'
-        }))
-        .pipe(gulp.dest('./dist'));
-});
-
-gulp.task('watch', function() {
-    gulp.watch('./src/assets/css/**/*.css', ['css']);
-    gulp.watch('./src/assets/js/**/*.js', ['js']);
-    gulp.watch('./src/**/*.hbs', ['html']);
-})
-
-gulp.task('default', ['css', 'js', 'html', 'webserver', 'watch']);
-
-gulp.task('clear', function() {
-    del([
-        './dist/*'
-    ]);
-});
